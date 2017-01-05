cfd179017c40cbb29778eb2f47cc736e029bc079
bngacli <cli@bnga.com>
2017-01-05 14:42:08 -0800

    test

---
+++ demo.py
@@ -0,0 +1,17 @@
+import urllib
+import urllib2
+
+values={}
+values['username'] = "defenyong@hotmail.com"
+values['password'] = "123"
+data = urllib.urlencode(values)
+url = "https://www.zhihu.com/?next=%2Finbox#signin"
+geturl = url +"?"+data
+request = urllib2.Request(geturl)
+response = urllib2.urlopen(request)
+print response.read()
+print geturl
+
+
+
+
